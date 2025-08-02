9a4ba5c52c60d90bc3470366dd5dd5761e50f87d
Giovanni Washel <giowashel@gmail.com>
2025-08-01 23:01:37 -0400

    add switching players

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var is_active := false
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
@@ -7,9 +7,11 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if not is_active:
+		return
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
---
+++ scripts/player_manager.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var characters = [];
+# Called when the node enters the scene tree for the first time.
+@onready
+var activeCharacter: CharacterBody2D = $GuitarHero; 
+const INACTIVE_OPACITY = 0.3
+const ACTIVE_OPACITY = 1.0
+const GUITAR_HERO_INDEX = 0
+const DRUMER_BOY_INDEX = 1
+const PIANO_MAN_INDEX = 2
+
+func _ready() -> void:
+	characters.append($GuitarHero)
+	characters.append($PianoMan)
+	characters.append($DrummerBoy)
+	for character in characters:
+		character.is_active = false
+	activeCharacter.modulate.a = ACTIVE_OPACITY
+	activeCharacter.is_active = true
+	$DrummerBoy.modulate.a = INACTIVE_OPACITY
+	$PianoMan.modulate.a = INACTIVE_OPACITY;
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_select_1"):
+		switch_character(GUITAR_HERO_INDEX)
+	elif Input.is_action_just_pressed("ui_select_2"):
+		switch_character(PIANO_MAN_INDEX)
+	elif Input.is_action_just_pressed("ui_select_3"):
+		switch_character(DRUMER_BOY_INDEX)
+	
+	
+func switch_character(index: int) -> void:
+	if index < 0 || index > 2:
+		return
+	activeCharacter.is_active = false
+	activeCharacter.modulate.a = INACTIVE_OPACITY
+	activeCharacter = characters[index]
+	activeCharacter.modulate.a = ACTIVE_OPACITY
+	activeCharacter.is_active = true
+	
+	# Lower the opacity of the previously active character
+ 
+	
+	
