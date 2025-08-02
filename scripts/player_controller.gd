5312eac26c239cb9b6b02cf8a06f60ee689f96b4
MichaelPeriod <39890684+MichaelPeriod@users.noreply.github.com>
2025-08-02 12:47:39 -0400

    Player manager refactored

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
-var is_active := false
+class_name Player_Controller
+
+var curr_dir : float
+var primary : bool
+var secondary: bool
 
 const SPEED = 100
 const JUMP_VELOCITY = -400.0
@@ -10,31 +14,19 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	# player selected	
-	if is_active:
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-			$Sprite2D.flip_h = (velocity.x < 0)
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := curr_dir
+	if direction:
+		velocity.x = direction * SPEED
+		$Sprite2D.flip_h = (velocity.x < 0)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	var previous_velocity = velocity; 
 	move_and_slide();
-	for i in range(get_slide_collision_count()):
-		var collision = get_slide_collision(i)
-		_handle_collision(collision, previous_velocity)
 		
-func _handle_collision(collision: KinematicCollision2D, old_velocity: Vector2):
-	velocity *= AIR_RESISTANCE
-	var collider = collision.get_collider();
-	# if we collide with another player
-	if collider is CharacterBody2D && collider != self:
-		var normal = collision.get_normal();
-		if is_on_floor():
-			velocity.x += normal.x * 15
-			velocity.y += normal.y * -10;
-		if collider.has_method("recieve_push"):
-			collider.call("recieve_push", Vector2(-normal.x * 15, -10))
-func recieve_push(push_velocity: Vector2):
-	velocity += push_velocity;
+
+func pass_input(_dir: float, _primary: bool, _secondary: bool):
+	curr_dir = _dir
+	primary = _primary
+	secondary = _secondary
--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
 var characters = [];
-# Called when the node enters the scene tree for the first time.
-@onready
-var activeCharacter: CharacterBody2D = $GuitarHero; 
-const INACTIVE_OPACITY = 0.3
-const ACTIVE_OPACITY = 1.0
+@export var INACTIVE_OPACITY = 0.3
+@export var ACTIVE_OPACITY = 1.0
+var active_index : int; 
 const GUITAR_HERO_INDEX = 0
 const DRUMER_BOY_INDEX = 1
 const PIANO_MAN_INDEX = 2
@@ -14,13 +12,21 @@ func _ready() -> void:
 	characters.append($GuitarHero)
 	characters.append($PianoMan)
 	characters.append($DrummerBoy)
-	for character in characters:
-		character.is_active = false
-	activeCharacter.modulate.a = ACTIVE_OPACITY
-	activeCharacter.is_active = true
-	$DrummerBoy.modulate.a = INACTIVE_OPACITY
-	$PianoMan.modulate.a = INACTIVE_OPACITY;
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+	active_index = GUITAR_HERO_INDEX
+	
+	update_opacities();
+
+func update_opacities():
+	for i in range(0, len(characters)):
+		if i == active_index:
+			characters[i].modulate.a = ACTIVE_OPACITY
+		else:
+			characters[i].modulate.a = INACTIVE_OPACITY
+
+func get_active_character() -> Player_Controller:
+	return characters[active_index]
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_select_1"):
 		switch_character(GUITAR_HERO_INDEX)
@@ -28,19 +34,15 @@ func _process(delta: float) -> void:
 		switch_character(PIANO_MAN_INDEX)
 	elif Input.is_action_just_pressed("ui_select_3"):
 		switch_character(DRUMER_BOY_INDEX)
-		
 	
+	var direction := Input.get_axis("left", "right")
+	var primary_action := Input.is_action_just_pressed("primary_action")
+	var secondary_action := Input.is_action_just_pressed("secondary_action")
 	
+	get_active_character().pass_input(direction, primary_action, secondary_action)
+
 func switch_character(index: int) -> void:
 	if index < 0 || index > 2:
 		return
-	activeCharacter.is_active = false
-	activeCharacter.modulate.a = INACTIVE_OPACITY
-	activeCharacter = characters[index]
-	activeCharacter.modulate.a = ACTIVE_OPACITY
-	activeCharacter.is_active = true
-	
-	# Lower the opacity of the previously active character
- 
-	
-	
+	active_index = index
+	update_opacities()
