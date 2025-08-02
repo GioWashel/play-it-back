81c55a583ff8975d35abd58758b3d19d9b518e4a
MichaelPeriod <39890684+MichaelPeriod@users.noreply.github.com>
2025-08-01 22:00:56 -0400

    Updated sprite flipping

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -13,8 +13,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+
 	if direction:
 		velocity.x = direction * SPEED
+
+		$Sprite2D.flip_h = (velocity.x < 0)
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
